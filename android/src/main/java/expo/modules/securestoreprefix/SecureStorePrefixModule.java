0ca96f9afffdc1cc8ace0ba7a2f318ac80294664
rearclab-arai <arai.yuma@re-arc-lab.jp>
2021-10-22 13:28:30 +0900

    コンパイルエラー

--- android/src/main/java/expo/modules/securestoreprefix/SecureStorePrefixModule.java
+++ android/src/main/java/expo/modules/securestoreprefix/SecureStorePrefixModule.java
@@ -1,16 +1,20 @@
 package expo.modules.securestoreprefix;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
+import org.unimodules.core.Promise;
 import org.unimodules.core.interfaces.ExpoMethod;
 
 import expo.modules.securestore.SecureStoreModule;
 
 
 public class SecureStorePrefixModule extends SecureStoreModule {
+  private static final String TAG = "ExpoSecureStorePrefix";
   private static final String SHARED_PREFERENCES_NAME = "SecureStore";
 
   private String mExperienceId;
@@ -22,7 +26,12 @@ public class SecureStorePrefixModule extends SecureStoreModule {
   @ExpoMethod
   @SuppressWarnings("unused")
   public void initAsync(String experienceId, Promise promise) {
-    mExperienceId = URLEncoder.encode(experienceId, "UTF-8");
+    try {
+      mExperienceId = URLEncoder.encode(experienceId, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      Log.e(TAG, "Caught unexpected exception when init from SecureStorePrefix", e);
+      promise.reject("E_SECURESTOREPREFIX_INIT_ERROR", "An unexpected error occurred when init from SecureStorePrefix", e);
+    }
   }
 
   protected SharedPreferences getSharedPreferences() {
