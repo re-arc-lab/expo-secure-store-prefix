47eaaf7470f5f0f584d8396a8900aace0ad50f31
rearclab-arai <69837389+rearclab-arai@users.noreply.github.com>
2021-10-22 13:13:43 +0900

    0.1.0 (#6)

--- android/src/main/java/expo/modules/securestoreprefix/SecureStorePrefixPackage.java
+++ android/src/main/java/expo/modules/securestoreprefix/SecureStorePrefixPackage.java
@@ -1,4 +1,4 @@
-package expo.modules.securestore;
+package expo.modules.securestoreprefix;
 
 import android.content.Context;
 
@@ -8,9 +8,9 @@ import java.util.List;
 import org.unimodules.core.BasePackage;
 import org.unimodules.core.ExportedModule;
 
-public class SecureStorePackage extends BasePackage {
+public class SecureStorePrefixPackage extends BasePackage {
   @Override
   public List<ExportedModule> createExportedModules(Context context) {
-    return Collections.singletonList((ExportedModule) new SecureStoreModule(context));
+    return Collections.singletonList((ExportedModule) new SecureStorePrefixModule(context));
   }
 }
